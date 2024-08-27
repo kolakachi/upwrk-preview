927748bd5954665bd19a60432bcacf8334ab2997
Kolawole <kolakachi@gmail.com>
2024-08-27 21:39:26 +0100

    update base blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <title>Vue Laravel Application</title>
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div id="app"></div> <!-- Vue.js will mount to this div -->
